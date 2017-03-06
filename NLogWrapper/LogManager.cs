22fa7e6399ee2b3c2d6187e17521bf46449d2d39
it-arjan <it-arjan@users.noreply.github.com>
2017-03-06 12:39:41 +0100

    v 1.2.1 add log level setting using a string

--- NLogWrapper/LogManager.cs
+++ NLogWrapper/LogManager.cs
@@ -6,15 +6,36 @@ namespace NLogWrapper
     public static class LogManager
     {
         public enum ILogLevel { Debug, Info, Error, Trace };
-        public static NLogWrapper.ILogger CreateLogger(Type T, ILogLevel level, string fallbackPath=null)
+        public static NLogWrapper.ILogger CreateLogger(Type T, string logLevel, string fallbackPath=null)
         {
-            //where to handle Nlog.LogLevel level
-            return new Logger(T, level, fallbackPath);
+            return new Logger(T, String2Enum(InitCap(logLevel)), fallbackPath);
         }
-        //old one
+        public static NLogWrapper.ILogger CreateLogger(Type T, ILogLevel logLevel, string fallbackPath = null)
+        {
+            return new Logger(T, logLevel, fallbackPath);
+        }
+        
+        // Most easy one
         public static NLogWrapper.ILogger CreateLogger(Type T)
         {
             return new Logger(T, ILogLevel.Debug);
+        }
+
+        private static ILogLevel String2Enum(string value)
+        {
+            ILogLevel result = ILogLevel.Debug;
+            Enum.TryParse(value, out result);
+            return result;
+        }
+        private static string InitCap(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            char[] a = s.ToCharArray();
+            a[0] = char.ToUpper(a[0]);
+            return new string(a);
         }
     }
 }
--- readme.txt
+++ readme.txt
@@ -7,22 +7,26 @@ This will work in Web & Windows ENV.
 The code is no big deal, AND PROBABLY NOT COMPLETE ;) https://github.com/it-arjan/NLog.ZeroConfig
 
 --------
--- Version 1.1 --
+-- Version 1.1.2 --
 ------------
 Usage example:
 
 using NLogWrapper;
-ILogger _logger = LogManager.CreateLogger(typeof(yourClass)); 
+
 // Log level will be Debug
 // Creates modulePath/logs or wwroot/logs
 // fallback log folder = Path.GetTempfolder()
+ILogger _logger = LogManager.CreateLogger(typeof(yourClass)); 
 
-ILogger _logger = LogManager.CreateLogger(typeof(yourClass), ILogLevel.Error)
-// allows to set log level
+// allows to set log level using a simple string, the casing does not matter
+ILogger _logger = LogManager.CreateLogger(typeof(yourClass), "Error")
 
 
-ILogger _logger = LogManager.CreateLogger(typeof(yourClass), ILogLevel.Error, "logging-fallback-folder-must-exist")
 // allows to set fallback log path
+ILogger _logger = LogManager.CreateLogger(typeof(yourClass), ILogLevel.Error, "logging-fallback-folder-must-exist")
+
+_logger.Debug("The focus is on {0} logging and on {1}", "RELIABLE", "convenience");
+_logger.Debug("a mistake in message parameter numbering will {1} throw an Exception but an error message in log file", "NOT");
 
 --------
 -- Version 1.0 --
